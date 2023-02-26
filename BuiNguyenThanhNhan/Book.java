ad390c8bcdcd46a69f9a06ab2443003da6b681f6
nhan81 <125139950+nhan81@users.noreply.github.com>
2023-02-26 20:44:03 +0700

    Add files via upload

---
+++ BuiNguyenThanhNhan/Book.java
@@ -0,0 +1,30 @@
+
+package bt;
+
+public class Book {
+    public String boCode;
+    public String Title;
+    public String boAuthor;
+    public Book(){}
+
+    public Book(String boCode, String Title, String boAuthor)
+    {
+        this.boCode = boCode;
+        this.Title = Title;
+        this.boAuthor = boAuthor;
+    }
+
+    public String getBoCode() {
+        return boCode;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public String getBoAuthor() {
+        return boAuthor;
+    }
+    
+    
+}
